76127845df40e066fd78474bbc54fca10086fb1d
Caio César Gallo <caio.gallo@gmail.com>
2015-11-30 17:03:17 -0200

    atualizacao script de instalação docker

--- docker-bootstrap.sh
+++ docker-bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get -y install docker.io
-sudo gpasswd -a vagrant docker
+#sudo apt-get -y install docker.io
+#sudo gpasswd -a vagrant docker
+wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker vagrant
